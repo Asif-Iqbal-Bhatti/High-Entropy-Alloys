1acb4f9d0c875e5ffeda2e12bf17e072923ddf72
Asif I.B <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-05-05 04:16:37 +0200

    Update BCCScrew_dislocation.f90

--- BCCScrew_dislocation.f90
+++ BCCScrew_dislocation.f90
@@ -13,11 +13,11 @@ program BCC_ScrewDislocation
 IMPLICIT NONE
 !------------------------- 
 integer                :: atom_cell, ncelltot, numatomstot, latt_par
-integer                :: i, j, k, l, image_cell, iun, m
-real(kind=8)           :: R_2, R_3, R_6, delta, d, P
+integer                :: i, j, k, l, image_cell, iun, m, p, q
+real(kind=8)           :: R_2, R_3, R_6, delta, r, d
 real(kind=8)           :: burgers, bcc_lat
 real(kind=8)           :: pivalue, factor
-real(kind=8)           :: radius, xx, yy, zz
+real(kind=8)           :: xx, yy, zz
 real(kind=8)           :: ux, uy, theta1,theta2,theta3,theta4
 real(kind=8)           :: d1, d2, d3, d4
 integer,dimension(4)   :: N
@@ -39,6 +39,7 @@ pivalue = 2.0d0*asin(1.0d0)
 !--- Enter the Ta or Nb lattice parameter obtained from DFT
 bcc_lat = 3.31953d0
 !------------------------- 
+call random_number(r)
 
 ! -- Definition of unit cell
 atom_cell = 6
@@ -65,11 +66,11 @@ supercell(1)=N(1)*unit_lat(1)
 supercell(2)=N(2)*unit_lat(2)
 supercell(3)=N(3)*unit_lat(3)
 
-write(*,*)'X=[112]',unit_lat(1),'Y=[110]', unit_lat(2)
-write(*,*)'Supercell scaling (X,Y,Z)', N(1), N(2), N(3)
-write(*,*)'Scaling unit cells along X,', supercell(1)
-write(*,*)'Scaling unit cells along Y,', supercell(2)
-write(*,*)'Scaling unit cells along Z,', supercell(3)
+print'("X=a*[112]; ", "Y=a*[110]; ", "Z=a*[111]/2; ", 3F12.6)',unit_lat(1), unit_lat(2),unit_lat(3) 
+print'("Supercell scaling N(X,Y,Z)", 3I9)', N(1), N(2), N(3)
+print'("Scaling unit cells along NX,", F15.9)', supercell(1)
+print'("Scaling unit cells along NY,", F15.9)', supercell(2)
+print'("Scaling unit cells along NZ,", F15.9)', supercell(3)
 
 ncelltot = N(1)*N(2)*N(3)
 allocate(Tot_atom(ncelltot,atom_cell,3))
@@ -90,30 +91,30 @@ do i=1,N(1)
  enddo
 enddo
 Tot_perfect = Tot_atom
-write(*,*) "Total Number of atoms in the cell is", image_cell*6
+print'("Total Number of atoms in the supercell", I8)', image_cell*6
 
-write(*,'(a)') ''   
-write(*,'(a)') 'Generating Screw Dislocations at Position of dislocation line >>>'
+print'(" ")'   
+write(*,'(a)') 'Generating Screw Dislocations (dislo) at Position of dislocation line >>>'
 !#***************Position of dislocation line at C1(X, Y, Z) for +b*********
 C1(:) = 0.0d0; delta = 0.001D-01 ! to avoid on top of atom
-C1(1) = 0.251d0*supercell(1) - delta !** X1
-C1(2) = 0.251d0*supercell(2) 				!** Y1
-C1(3) = supercell(3) 						!** Z1
+C1(1) = 0.251d0*supercell(1) - delta 
+C1(2) = 0.251d0*supercell(2) 			
+C1(3) = supercell(3) 					
 !#***************Position of dislocation line at C2(X, Y, Z) for -b*********
-C2(:) = 0.0d0; delta = 0.001D-01 ! to avoid on top of atom
-C2(1) = 0.751d0*supercell(1) + delta !** X2
-C2(2) = 0.251d0*supercell(2) 				!** Y2
-C2(3) = supercell(3) 				!** Z2
+C2(:) = 0.0d0;
+C2(1) = 0.751d0*supercell(1) + delta
+C2(2) = 0.251d0*supercell(2) 				
+C2(3) = supercell(3) 				
 !#***************Position of dislocation line at C3(X, Y, Z) for -b*********
-C3(:) = 0.0d0; delta = 0.001D-01 ! to avoid on top of atom
-C3(1) = 0.251d0*supercell(1) - delta !** X2
-C3(2) = 0.751d0*supercell(2) 				!** Y2
-C3(3) = supercell(3) 				!** Z2
+C3(:) = 0.0d0; 
+C3(1) = 0.251d0*supercell(1) - delta 
+C3(2) = 0.751d0*supercell(2) 				
+C3(3) = supercell(3) 				
 !#***************Position of dislocation line at C4(X, Y, Z) for +b*********
-C4(:) = 0.0d0; delta = 0.001D-01 ! to avoid on top of atom
-C4(1) = 0.751d0*supercell(1) + delta !** X2
-C4(2) = 0.751d0*supercell(2) 				!** Y2
-C4(3) = supercell(3) 				!** Z2
+C4(:) = 0.0d0;
+C4(1) = 0.751d0*supercell(1) + delta 
+C4(2) = 0.751d0*supercell(2) 				
+C4(3) = supercell(3) 			
 
 print'("C1(X,Y,Z) for +b",3F14.6)', C1(1),C1(2),C1(3)
 print'("C2(X,Y,Z) for -b",3F14.6)', C2(1),C2(2),C2(3)
@@ -124,13 +125,23 @@ print'("C4(X,Y,Z) for -b",3F14.6)', C4(1),C4(2),C4(3)
 d1 = sqrt( (C1(1)-C2(1))**2 + (C1(2)-C2(2))**2 )
 d2 = sqrt( (C1(1)-C3(1))**2 + (C1(2)-C3(2))**2 )
 d3 = sqrt( (C1(1)-C4(1))**2 + (C1(2)-C4(2))**2 )
-print'("*** Distance between Dipoles >>> ",F9.6 )', d1
-print'("*** Distance between Dipoles >>> ",F9.6 )', d2
-print'("*** Distance between Dipoles >>> ",F9.6 )', d3
-print'("Edge lattice constructing from unit cell >>> ")'
-print'("*** h1 = 7e1 >>> ",F9.6)'                , 7*unit_lat(1)
-print'("*** h2 = 3.5e1+5.5e2+0.5e3 >>> ",3F14.6)', 3.5*unit_lat(2), 5.5*unit_lat(2), 0.5*unit_lat(2)
-print'("*** h3 = 2e3 >>> ",F9.6)'                , 2*unit_lat(3) ! For HEAs twice the thickness
+print'("d1=|C1-C2|, d2=|C1-C3|, d3=|C1-C4| >>> ",3F12.6 )', d1, d2, d3
+print'("-Dist along NX-d1,(NX-d1)/2 >>> ",2F14.6 )', supercell(1)-d1, (supercell(1)-d1)/2
+print'("-Dist along NY-d2,(NY-d2)/2 >>> ",2F14.6 )', supercell(2)-d2, (supercell(2)-d2)/2
+print'(" ",2F14.6 )', C1(1) - sqrt( 28.459**2 - (C1(2) - 38.7815)**2 )
+
+print'("-------------------------------------------------------------------------")'
+print'("Constructing Edge lattice from unit cell >>> ")'
+print'("Ju Li et al paper")'
+print'("-> h1 = 7e1 >>> ",F20.6)'               , 7*unit_lat(1)
+print'("-> h2 = 3.5e1+5.5e2+0.5e3 >>> ",3F14.6)', 3.5*unit_lat(2), 5.5*unit_lat(2), 0.5*unit_lat(2)
+print'("-> h3 = 2e3 >>> ",F20.6)'               , 2*unit_lat(3) ! For HEAs twice the thickness
+p=15; q=9
+print'("L. Ventelon et al paper")'
+print'("-> C1 = pe1-(1/3q)e3 >>> ",2F20.6)' , p*unit_lat(1), -(1/(3*q))*unit_lat(3)
+print'("-> C2 = 0.5pe1+qe2+(0.5-1/6q)e3 >>> ",3F14.6)', 0.5*p*unit_lat(1), q*unit_lat(2), (0.5-1/(6*q))*unit_lat(3)
+print'("-> C3 = e3 >>> ",F20.6)'               , unit_lat(3) ! For HEAs twice the thickness
+print'("-------------------------------------------------------------------------")'
 
 !----------------------------------------------------------------------------
 !A periodic array is quadrupole, if the vector d linking the two disloca-
@@ -139,15 +150,13 @@ print'("*** h3 = 2e3 >>> ",F9.6)'                , 2*unit_lat(3) ! For HEAs twic
 !metry center of the array: fixing, as a convention, the origin at a dislocation center,
 !if a dislocation b is located at the position r, there will also be a dislocation b in −r.
 
-d = 0.50d0*(supercell(1) + supercell(2))
-write(*,*) 'Vector d linking the two dislocations of opposite signs is >>> ', d
-p = sqrt( d**2 - (C2(1)-C1(1))**2 )
+d = (supercell(1) + supercell(2))/2.0
+print'("Vector d linking the two +b & -b  >>> ",2F14.6)', d, d-d1
 
 L0=0.0; L1=0.0
 L0(1) = supercell(1)
 L1(2) = supercell(2)
 diag = 0.5*(dot_product(L0,L0)+dot_product(L0,L1))/(dot_product(L0,L0)+dot_product(L1,L1)+4*dot_product(L0,L1))
-write(*,*) diag
 	
 ! --------------------Screw dislocation displacement------------------------
 burgers = bcc_lat*R_3/2.0 ! b=a<111>/2
@@ -202,6 +211,10 @@ write(1,*) dimbox(1,1:2),' xlo xhi'
 write(1,*) dimbox(2,1:2),' ylo yhi'
 write(1,*) dimbox(3,1:2),' zlo zhi'
 write(1,*)
+write(1,*) 'Masses'
+write(1,*)
+write(1,*) '1 180.94788  # Ta'
+write(1,*)
 write(1,*) 'Atoms'
 write(1,*)
 m=1
@@ -221,6 +234,7 @@ enddo
 close(1)
 
 !#********************* POSCARScrew_unrelaxed *****************************
+print'(" >>> Files have been generated ... <<< ")'
 open(2,file='POSCARScrew_unrelaxed',status='REPLACE')
 write(2,'(a)') 'Ta screw'
 write(2,'(F9.6)') bcc_lat ! bcc_lat = 3.31953d0
