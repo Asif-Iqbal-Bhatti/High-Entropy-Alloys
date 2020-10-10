8f51deb5bd63c6923c3b02c018779a0b7487cd07
Asif <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-10-10 21:41:20 +0200

    Update configuration_entropy.py
    This script heavily depends on the way the other codes generate the data.

--- configuration_entropy.py
+++ configuration_entropy.py
@@ -12,37 +12,57 @@ k = 8.617333262145E-5 # Boltzmann constant eV/K
 T = 300 # Temperature in Kelvin
 z = 14 # Nearest neighbor 1st=4N, 2nd=3N, N is the number of lattice points
 ci = [1/5,1/5,1/5,1/5,1/5] # concentration of each element in a alloy
-print ("This script calcutes the ")
+
 # Dictionary is defined for pairing probabilities. These values are taken from SRO.
-dict = {'Nb-Ta':-0.05,
-				'Nb-Ti':0.0,
+e1 = 0 ; e2 = 0 ; dd = []
+os.remove("sro_tmp.dat")
+dict = {'Nb-Ta':0.05,
+				'Nb-Ti':0.0002,
 				'Nb-Zr':0.1,
 				'Nb-Hf':0.05,
 				'Ti-Zr':0.125,
-				'Ti-Ta':-0.025,
-				'Ti-Hf':-0.15,
+				'Ti-Ta':0.025,
+				'Ti-Hf':0.15,
 				'Ta-Zr':0.025,
-				'Hf-Zr':-0.05,
+				'Hf-Zr':0.05,
 				'Hf-Ta':0.1,
 				'Nb-Nb':0.45,
 				'Ta-Ta':0.475,
 				'Ti-Ti':0.525,
 				'Hf-Hf':0.525,
 				'Zr-Zr':0.4}
+##################################################################################				
 
-for i in range (5):
-	e1 = ( ci[i]*np.log(ci[i]) - ci[i] )
+sro_tmp = os.popen("sqsgenerator alpha sqs POSCAR --weight=1,0.5 --verbosity=3 " ).read()
+output = open("sro_tmp.dat", 'w')
+for line in sro_tmp:
+	output.write(line)
+output.close()
+with open("sro_tmp.dat") as fp:
+	line = fp.readline()
+	cnt = 1
+	while line:
+		#print("{}".format(line.strip(" ").strip("\n").strip(";").split("=") ))
+		dd.append( line.strip(" ").strip("\n").strip(";").split("=") )
+		cnt +=1
+		line = fp.readline()
+##################################################################################
+
+for i in range (len(ci)):
+	e1 = e1 + ( ci[i]*np.log(ci[i]) - ci[i] )
 l1 = (z-1) * e1	
-#print ( l1 )
-for x in dict:
-	e2 = ( dict[x]*np.log(dict[x]) - dict[x] )
-l2 = z * e2
-#print ( l2 )
 
+#for x in dict:
+#	e2 = e2 + ( dict[x]*np.log(dict[x]) - dict[x] )
+#l2 = z/2 * e2
+
+for i in range(cnt-2):
+	e2 = e2 + ( float(dd[i+1][1]) * np.log(float(dd[i+1][1])) - float(dd[i+1][1]) )
+	#print (dd[i+1][0], float(dd[i+1][1]))
+l2 = z/2 * e2	
+	
 # Final entropy of the system :
 for x in dict:
-	print ("{:6s} {:9.5f}".format( x, dict[x] ) )
-print (  "-TS = -{:12.9f} meV for T=300K".format( (l1 - l2 + ( z/2 - 1 ))*k*1000*T    ))
-
-
+	print ("{:6s} {:9.5f}".format( x, dict[x] ), end='\r' )
+print (  "T*S = {:12.5f} meV at T=300K".format( (l1 - l2 + ( z/2 - 1 ))*k*1000*T    ))
 
