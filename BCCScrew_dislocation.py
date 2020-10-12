0b99bf3b2a0b3c2ecd86534c05a9513822e65867
Asif <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-10-12 15:01:47 +0200

    Update BCCScrew_dislocation.py

--- BCCScrew_dislocation.py
+++ BCCScrew_dislocation.py
@@ -2,12 +2,13 @@
 
 '''!----------------------------------------------------------------------------
 !----------------------------------------------------------------------------
-!!! USAGE ::: ./python3 <fcc/bcc> <c/d> <sup/screw>
+!!! USAGE ::: python3 <fcc/bcc> <c/d> <sup/screw>
 !!! AUTHOR::: Asif Iqbal
-!!! DATED ::: 30/04/2020
+!!! DATED ::: 12/10/2020
 !!! GITHUB::: @asif_em
 !!! USE AT YOUR OWN RISK. NOT EVEN IMPLIED WARRANTY WHATSOEVER
 !!! CAREFULLY CHECK THE GEOMETRY BEFORE SUBMITTING TO DFT CALCULATION.
+!!! This script creates a screw dislocations in a BCC supercell
 !----------------------------------------------------------------------------
 !----------------------------------------------------------------------------'''
 
@@ -21,8 +22,8 @@ from numpy import linalg as LA
 # 131–138 (2014).
 pivalue = 2.0*np.arcsin(1.0) 
 lat_par = 3.405 # lattice parameter
-Nx = 11; 
-Ny = 15;
+Nx = 7; 
+Ny = 11;
 Nz = 1
 #----------------------------------------------------------------------------------- 
 
@@ -32,16 +33,16 @@ def HEAs_supercell():
 	SZ = 5
 	cart_atoms=[]; count=0
 	lat_vec = np.array([[1,0,0],
-			[0,1,0],
-			[0,0,1]])*lat_par
+											[0,1,0],
+											[0,0,1]])*lat_par
 	
 	lattice_bcc = np.array([[0,0,0],
-		[0.5,0.5,0.5]])*lat_par
+												[0.5,0.5,0.5]])*lat_par
 						
 	lattice_fcc = np.array([[0,0,0],
-			[0.0,0.5,0.5],											
-			[0.5,0.0,0.5],											
-			[0.5,0.5,0.0]])*lat_par										
+												[0.0,0.5,0.5],											
+												[0.5,0.0,0.5],											
+												[0.5,0.5,0.0]])*lat_par										
 	fcc = lattice_fcc; 
 	if (sys.argv[1]=='fcc'): b = lat_par*np.sqrt(2)/2.0; print("burger vector::", b)
 	bcc = lattice_bcc; 
@@ -125,7 +126,7 @@ def BCCgen_111_perfect():
 	return pos_perfect, count, basis, lx, ly, lz
 
 '''##################################################################################
-#------------------ Creating screw dislocation for randomise HEA -------------------#
+#------------------ Creating screw dislocation for randomize HEA -------------------#
 ##################################################################################'''	
 
 def creating_screw_perfect(rand_atoms, count, basis, lx, ly, lz):	
