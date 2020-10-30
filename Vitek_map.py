73456d5cbb66708079858b071205ec8f6e612668
Asif <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-10-30 14:47:04 +0100

    Update Vitek_map.py

--- Vitek_map.py
+++ Vitek_map.py
@@ -3,15 +3,16 @@
 # AUTHOR:: ASIF IQBAL
 # USAGE :: python3 sys.argv[0] <perf/final>
 # NB :: To plot Differential displacement map. ADAPTATION FROM CLOUET and ATOMMAN et al.,
-#https://en.wikipedia.org/wiki/Fractional_coordinates#Conversion_to_cartesian_coordinates
-#http://www.ruppweb.org/Xray/tutorial/Coordinate%20system%20transformation.htm
+# https://en.wikipedia.org/wiki/Fractional_coordinates#Conversion_to_cartesian_coordinates
+# http://www.ruppweb.org/Xray/tutorial/Coordinate%20system%20transformation.htm
 #-----------------------------------------------------------------------------------------
 
 import numpy as np
 import os, sys, random, subprocess, shutil, math
 
 Struct = sys.argv[1] # Define the reference structure
-
+global dict
+	
 def read_POSCAR_perfect():
 	P_pos = []; sum = 0; dict = {}; g = 0
 	reffile = open('POSCAR_perfect','r')
@@ -28,10 +29,10 @@ def read_POSCAR_perfect():
 	for x in range(int(P_atoms)):	
 		P_pos.append( [ str(i) for i in reffile.readline().rstrip(" ").split()[0:3] ] )
 	reffile.close() 
-	#                      !!! TURN THIS ON IF YOU WANT A FILE IN XYZ FORMAT !!!
+	### TURN THIS ON IF YOU WANT A FILE IN XYZ FORMAT 
 	for j in range( len( P_elemtype.split() )):
 		dict[P_elemtype.split()[j]] =  P_atomtypes.split()[j]; 
-	print (dict)
+	#print (dict)
 	#for l in dict:
 	#	for k in range( int(dict[l]) ):
 	#		#print( elementtype.split()[ math.floor( k/int(atomtypes.split()[0] ) ) ], pos[k][:] )
@@ -68,12 +69,9 @@ def Conversion_2_cartesian_coordinates(LV1,LV2,LV3):
 	for ai in LV1: a.append(float(ai)); x = np.linalg.norm(a)
 	for bi in LV2: b.append(float(bi)); y = np.linalg.norm(b)
 	for ci in LV3: c.append(float(ci)); z = np.linalg.norm(c)
-	### gamma = Cos-1( (a.b)/||a||.||b|| )
-	### alpha = Cos-1( (b.c)/||b||.||c|| )
-	### beta  = Cos-1( (a.c)/||a||.||c|| )
-	P_gamma = math.acos(np.dot(a,b) / (x * y))
-	P_alpha = math.acos(np.dot(b,c) / (y * z))
-	P_beta  = math.acos(np.dot(a,c) / (x * z))
+	P_gamma = math.acos(np.dot(a,b) / (x * y)); # gamma = Cos-1( (a.b)/||a||.||b|| ) 
+	P_alpha = math.acos(np.dot(b,c) / (y * z)); # alpha = Cos-1( (b.c)/||b||.||c|| )
+	P_beta  = math.acos(np.dot(a,c) / (x * z)); # beta  = Cos-1( (a.c)/||a||.||c|| )
 	V = x*y*z * ( np.sqrt(1 + 2 * math.cos(P_alpha) * math.cos(P_beta) * \
 	math.cos(P_gamma) - math.cos(P_alpha)**2 - math.cos(P_beta)**2 - math.cos(P_gamma)**2) )	
 	M = [ [x, y * math.cos(P_gamma), z * math.cos(P_beta)],
@@ -81,26 +79,7 @@ def Conversion_2_cartesian_coordinates(LV1,LV2,LV3):
 			[0, 0, V / (x * y * math.sin(P_gamma)) ] ]
 	return M
 
-#---------------------------------- MAIN ENGINE ------------------------------------
-
-if __name__ == "__main__":
-	P_atoms,P_pos,P_comment,P_alat,P_LV1,P_LV2,P_LV3,P_elemtype,P_atomtypes,P_Coordtype = read_POSCAR_perfect();	
-	C_atoms,C_pos,C_comment,C_alat,C_LV1,C_LV2,C_LV3,C_elemtype,C_atomtypes,C_Coordtype = read_CONTCAR();	
-
-	# CHECKING IF THE ATOMS AND ELEMENTS ARE EQUAL in both files
-	if (P_atoms == C_atoms):
-		print ("atoms are equal")
-	else:
-		exit("atoms are not equal")	
-	if (P_elemtype.split()  == C_elemtype.split() ):
-		print ("Elements are equal")
-	else:
-		exit("Elements are not equal")
-	if ( P_Coordtype[0] == C_Coordtype[0] ):
-		print ("Coordinates types are equal")
-	else:
-		exit("Coordinates types are not equal")
-
+def Write_to_file(P_atoms,P_LV1,P_LV2,P_LV3,C_LV1,C_LV2,C_LV3):
 	#----------------------!!! WRITING TO A DISPLACEMENT FILE !!! ----------------------
 	outfile = open("displacement.yaml", 'w')
 	outfile.write("{:5d}\n".format(P_atoms) )
@@ -150,6 +129,28 @@ if __name__ == "__main__":
 				format(Xc, Yc, Zc, Xc-Xp, Yc-Yp, Zc-Zp  ) )	
 	
 	outfile.close()
+	
+#---------------------------------- MAIN ENGINE ------------------------------------
+
+if __name__ == "__main__":
+	P_atoms,P_pos,P_comment,P_alat,P_LV1,P_LV2,P_LV3,P_elemtype,P_atomtypes,P_Coordtype = read_POSCAR_perfect();	
+	C_atoms,C_pos,C_comment,C_alat,C_LV1,C_LV2,C_LV3,C_elemtype,C_atomtypes,C_Coordtype = read_CONTCAR();	
+
+	### CHECKING IF THE ATOMS AND ELEMENTS ARE EQUAL in both files
+	if (P_atoms == C_atoms):
+		print ("atoms are equal")
+	else:
+		exit("atoms are not equal")	
+	if (P_elemtype.split()  == C_elemtype.split() ):
+		print ("Elements are equal")
+	else:
+		exit("Elements are not equal")
+	if ( P_Coordtype[0] == C_Coordtype[0] ):
+		print ("Coordinates types are equal")
+	else:
+		exit("Coordinates types are not equal")
+
+	Write_to_file(P_atoms,P_LV1,P_LV2,P_LV3,C_LV1,C_LV2,C_LV3)
 	
 	
 	
