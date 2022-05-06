00d3a5dca9507c924d53a06ebabd46262112de6b
Asif_rm <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2022-05-06 11:37:31 +0200

    Update SRO_pyscal.py

--- SRO_pyscal.py
+++ SRO_pyscal.py
@@ -1,57 +1,43 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 #
 # USING pyscal to compute SRO parameters using fontain et al.
 # https://pyscal.org/en/latest/_modules/pyscal/core.html#System.calculate_pmsro
-#
-import sys, os
-import pyscal.core as pc
+# https://pyscal.org/en/latest/pyscal.html?highlight=pmsro#pyscal.core.System.calculate_pmsro website? 
+import sys
+import pyscal as pc
+import pyscal.crystal_structures as pcs
+from ase import Atoms
 from ase.io.vasp import write_vasp, read_vasp
-from tqdm import tqdm
+
+# Read initial and final states:
+initial = read_vasp(sys.argv[1])
+pos = initial.get_positions()
+ucell = initial.get_cell()
+
+type = initial.get_chemical_symbols()
+atom_type = sorted(list(set(type)))
+natom = len(atom_type)
+print ("Order of the atom types and total", atom_type, natom)
 
 top = pc.System()
-cwd = os.path.abspath(os.path.dirname(__file__)) 
-with open("SRO_column.dat", "w") as f:
-	for k in next(os.walk('.'))[1]:
-		d = os.path.join(cwd, k)
+top.read_inputfile(sys.argv[1],format='poscar')
+top.find_neighbors(method='cutoff', cutoff=1.2)
 
-		top.read_inputfile(os.path.join(d, 'POSCAR_bestsqs'), format='poscar')
-		top.find_neighbors(method='cutoff', cutoff=1.2)
-		
-		initial = read_vasp(os.path.join(d, 'POSCAR_bestsqs'))
-		at_type = initial.get_chemical_symbols()
-		atom_type = sorted(list(set(at_type)))
-		print(k, atom_type,top.get_concentration())
-		
-		a=0; b=0; c=0; pairs_name = []; sro_1st_shell = []; sro_2st_shell = []; sro_1st_2nd_shell = []
-		f.write("{}\n".format(k))
-		f.write("{:6s}  {:10.10s} {:10.10s} {:10.10s}\n".format("Pairs", "1st Shell", "2nd Shell", "1st+2nd"))
-		
-		for t in range(5):
-			for j in range(5):
-				sro = top.calculate_pmsro(reference_type=t+1, compare_type=j+1, average=True, shells=2, delta=True)		
-				if (j > t):
-					f.write("{}-{} {:9.5f} {:9.5f} {:9.5f}\n".format(atom_type[t], atom_type[j], sro[0], sro[1], sro[0]+sro[1]))
-					pairs_name.append(atom_type[t]+"-"+atom_type[j])
-					sro_1st_shell.append(round(sro[0],4))
-					sro_2st_shell.append(round(sro[1],4))
-					sro_1st_2nd_shell.append(round(sro[0]+sro[1],4))
-					a=a+sro[0]
-					b=b+sro[1]
-					c=c+sro[0]+sro[1]
-				if (j == t):
-					f.write("{}-{} {:9.5f} {:9.5f} {:9.5f}\n".format(atom_type[t], atom_type[j], sro[0], sro[1], sro[0]+sro[1]))
-					pairs_name.append(atom_type[t]+"-"+atom_type[j])
-					sro_1st_shell.append(round(sro[0],4))
-					sro_2st_shell.append(round(sro[1],4))
-					sro_1st_2nd_shell.append(round(sro[0]+sro[1],4))	
-					a=a+sro[0]
-					b=b+sro[1] 			
-					c=c+sro[0]+sro[1]
-	
-		f.write("{}\n".format("-"*35))
-		f.write ("{:5s} {:9.5f} {:9.5f} {:9.5f}\n".format('T_SRO', a, b, a+b))
-		f.write("{}\n".format(pairs_name))
-		f.write("{}\n".format(sro_1st_shell))
-		f.write("{}\n".format(sro_2st_shell))
-		f.write("{}\n".format(sro_1st_2nd_shell))
-		f.write("{}\n".format("-"*50))
+a=0; b=0; c = 0
+print("{:6s}  {:10.10s} {:10.10s} {:10.10s}".format( "Pairs", "1st Shell", "2nd Shell", "1st+2nd"  ) )
+for t in range(natom):
+	for j in range(natom):
+		sro = top.calculate_pmsro(reference_type=t+1, compare_type=j+1, average=True, shells=2, delta=True)
+		if (j > t):
+			print("{}-{}, {:9.5f} {:9.5f} {:9.5f}".format( atom_type[t], atom_type[j], sro[0], sro[1], sro[0]+sro[1]  ) )
+			a=a+sro[0]
+			b=b+sro[1]
+			c=c+sro[0]+sro[1]
+		if (j == t):
+			print("{}-{}, {:9.5f} {:9.5f} {:9.5f}".format( atom_type[t], atom_type[j], sro[0], sro[1], sro[0]+sro[1]   ) )
+			a=a+sro[0]
+			b=b+sro[1] 			
+			c=c+sro[0]+sro[1]
+
+print ("Total SRO of the system {:9.5f}".format(a+b))
+
