368b30d7bb1cac2df67c06c2afc2ebb7b56bd258
Asif_rm <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2022-09-24 14:43:54 +0200

    Update create_bar_chat_plot.py

--- create_bar_chat_plot.py
+++ create_bar_chat_plot.py
@@ -3,12 +3,17 @@
 '''
 # NOTE:: skript voor bar plot X vs Y
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.bar.html
+# https://github.com/xnx/pyvalem
+# ASE object avoids the rendering effect !!!
 # FIRST CONVERT => *.txt or *.dat into csv file
 '''
 
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
-	
+import seaborn as sns
+from ase.formula import Formula
+
 a = {}
 subscript = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 
@@ -17,13 +22,11 @@ dg = pd.read_csv('X_vs_Y.csv')
 naam = list(dg.columns)
 
 for j in range(len(dg)):
-	a[j] = [dg[naam[0]][j].translate(subscript)] + [dg[naam[1]][j]]
-
-df = pd.DataFrame(a).T
-df.columns = naam
+	a[j] = [f'{Formula(dg[naam[0]][j]):latex}'] + [dg[naam[1]][j]]
+	
+df = pd.DataFrame(a).T; df.columns = naam
 df = df.sort_values(by=naam[1], ascending=False)
-
-df[naam[1]] = df[naam[1]].astype(float)
+#df[naam[1]] = df[naam[1]].astype(float)
 print(df)
 
 #====== WRITING TO A FILE
@@ -33,4 +36,9 @@ ax.set_ylabel(naam[1], rotation=90,fontweight='light',fontsize='medium')
 
 ax.figure.savefig('X_vs_Y-bar.png', dpi=400, bbox_inches='tight', pad_inches=0)
 df.to_csv(f'{"X_vs_Y-out"}.csv', encoding='utf-8')
-plt.show()
+
+#====== SAEBORN PLOTTING!!!
+tt = sns.barplot(data=df, x="Formula", y="voltage", color='b', ci=None)
+fig = tt.get_figure()
+fig.savefig("out.png", dpi=400, bbox_inches='tight', pad_inches=0)
+
