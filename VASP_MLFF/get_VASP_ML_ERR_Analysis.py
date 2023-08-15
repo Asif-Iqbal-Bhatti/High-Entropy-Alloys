627edcb1ad16662e653f394d0ee00a6fb444cfb2
Asif_em2r <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2023-08-15 21:19:28 +0200

    Update get_VASP_ML_ERR_Analysis.py

--- VASP_MLFF/get_VASP_ML_ERR_Analysis.py
+++ VASP_MLFF/get_VASP_ML_ERR_Analysis.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 '''
-###
+##############################################################################
 # ML error analysis
 # AIB_EM for VASP.6.4.1
-###
+##############################################################################
 '''
 
 import os, sys
@@ -13,40 +13,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 os.system("grep T OSZICAR > frameVsEne.dat")
+file_path = 'frameVsEne.dat'
 df = pd.read_csv(file_path, delim_whitespace=True, header=None)
-minV = df[4].idxmin()
-print(minV)
-print((df.iloc[minV]).T)
+#df.index = range(1, len(df) + 1)
+print(df)
+df.to_csv('frameVsEne.csv')
+min_config = df[4].idxmin()
+print('MINIMUM INDEX :: ', min_config,'\n', df.iloc[min_config].T)
 
-######
+#######################  BEEF #######################
 os.system("cat ML_LOGFILE | grep BEEF > BEEF.dat")
 with open('BEEF.dat', 'r') as file:
     header_line = file.readlines()[11].strip()
-columns = header_line.split()[2:]
-
+columns1 = header_line.split()[2:]
+#print(columns1)
 data = np.genfromtxt('BEEF.dat', skip_header=14, usecols=range(1, 8),)
-df = pd.DataFrame(data,)
-df.columns = columns
-df.plot(x='nstep')
-plt.savefig('BEEF.png', bbox_inches='tight', dpi=300)
+df1 = pd.DataFrame(data, columns=columns1)
+df_filtered = df1[df1['nstep'] >= 100]
+df_filtered.plot(x='nstep')
+plt.savefig('BEEF.pdf', bbox_inches='tight', dpi=300)
 plt.clf()
-
-######
+    
+####################### ERR #######################
 os.system("cat ML_LOGFILE | grep ERR > ERR.dat")
 with open('ERR.dat', 'r') as file:
     header_line = file.readlines()[8].strip()
-columns = header_line.split()[2:]
-
+columns2 = header_line.split()[2:]
+#print(columns2)
 data = np.genfromtxt('ERR.dat', skip_header=11, usecols=range(1, 5),)
-df = pd.DataFrame(data,)
-df.columns = columns
-df.plot(x='nstep')
-plt.savefig('ERR.png', bbox_inches='tight', dpi=300)
+df2 = pd.DataFrame(data, columns=columns2)
+df_filtered = df2[df2['nstep'] >= 100]
+df_filtered.plot(x='nstep')
+plt.savefig('ERR.pdf', bbox_inches='tight', dpi=300)
 plt.clf()
 
-######
-os.system("grep BEEF ML_LOGFILE|grep -v '#'|awk '{print $2, $6}' > CTIFOR.dat")
-data = np.genfromtxt('CTIFOR.dat')
-df = pd.DataFrame(data,)
-df.plot(x=0)
 
